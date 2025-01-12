ba9449bbe35dec657c7ea6695a7d3534468ad10a
nenadjakic <nenad.jakic@gmail.com>
2025-01-12 12:22:28 +0100

    Removed unused imports.

--- server/src/main/kotlin/com/github/nenadjakic/keeptimer/server/Application.kt
+++ server/src/main/kotlin/com/github/nenadjakic/keeptimer/server/Application.kt
@@ -1,17 +1,9 @@
 package com.github.nenadjakic.keeptimer.server
 
-import com.github.nenadjakic.keeptimer.Greeting
 import com.github.nenadjakic.keeptimer.SERVER_PORT
-import com.mongodb.kotlin.client.coroutine.MongoClient
-import io.ktor.serialization.gson.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import io.ktor.server.plugins.*
-import io.ktor.server.plugins.contentnegotiation.*
-import org.koin.ktor.plugin.Koin
 
 fun main() {
     embeddedServer(Netty, port = SERVER_PORT, host = "0.0.0.0", module = Application::module)
--- shared/src/commonMain/kotlin/com/github/nenadjakic/keeptimer/domain/entity/Timer.kt
+++ shared/src/commonMain/kotlin/com/github/nenadjakic/keeptimer/domain/entity/Timer.kt
@@ -1,10 +1,7 @@
 package com.github.nenadjakic.keeptimer.domain.entity
 
 import kotlinx.serialization.Serializable
-import kotlin.uuid.ExperimentalUuidApi
-import kotlin.uuid.Uuid
 
-@OptIn(ExperimentalUuidApi::class)
 @Serializable
 data class Timer(
     val id: Long? = null,
